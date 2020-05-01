6a7b2d6127bab52c36a705c000a2a0f6d3388640
Philipp Seiler <p.seiler@linuxmail.org>
2020-05-01 14:52:21 +0200

    code improvements
    * made lists of values for parameters more puppet-like (use arrays)
    * template cleanup and generalizing
    * added octal checks for umask
    * added explicit typing for the remaining parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -25,60 +25,61 @@ class vsftpd (
   Optional[Boolean] $anon_mkdir_write_enable  = undef,
   Optional[Boolean] $anon_other_write_enable  = undef,
   Optional[Boolean] $anon_world_readable_only = undef,
-  $anon_umask                                 = undef,
-  $anon_root                                  = undef,
-  $ftp_username                               = undef,
+  Optional[String] $anon_umask                = undef,
+  Optional[String] $anon_root                 = undef,
+  Optional[Integer] $anon_max_rate            = undef,
+  Optional[String] $ftp_username              = undef,
   Optional[Boolean] $guest_enable             = undef,
-  $guest_username                             = undef,
-  $ftpd_banner                                = undef,
-  $banner_file                                = undef,
+  Optional[String] $guest_username            = undef,
+  Optional[String] $ftpd_banner               = undef,
+  Optional[String] $banner_file               = undef,
   Optional[Boolean] $local_enable             = undef,
   Optional[Boolean] $download_enable          = undef,
   Optional[Boolean] $write_enable             = undef,
-  $local_umask                                = undef,
+  Optional[String] $local_umask               = undef,
   Optional[Boolean] $chown_uploads            = undef,
-  $chown_username                             = undef,
+  Optional[String] $chown_username            = undef,
   Optional[Boolean] $dirmessage_enable        = undef,
   Optional[Boolean] $delete_failed_uploads    = undef,
   Optional[Boolean] $xferlog_enable           = undef,
   Optional[Boolean] $connect_from_port_20     = undef,
-  $xferlog_std_format                         = undef,
+  Optional[Boolean] $xferlog_std_format       = undef,
   Optional[Boolean] $chroot_local_user        = undef,
   Optional[Boolean] $chroot_list_enable       = undef,
-  $chroot_list_file                           = undef,
-  $cmds_allowed                               = undef,
-  $cmds_denied                                = undef,
-  $deny_file                                  = undef,
-  $hide_file                                  = undef,
+  Optional[String] $chroot_list_file          = undef,
+  Optional[Array[Vsftpd::Cmd]] $cmds_allowed  = undef,
+  Optional[Array[Vsftpd::Cmd]] $cmds_denied   = undef,
+  Optional[Array[String]] $deny_file          = undef,
+  Optional[Array[String]] $hide_file          = undef,
   Optional[Boolean] $hide_ids                 = undef,
-  $file_open_mode                             = undef,
-  $ftp_data_port                              = undef,
+  Optional[String] $file_open_mode            = undef,
+  Optional[Integer] $ftp_data_port            = undef,
   Optional[Boolean] $listen                   = undef,
   Optional[Boolean] $listen_ipv6              = undef,
-  $listen_port                                = undef,
-  $local_max_rate                             = undef,
-  $pam_service_name                           = undef,
+  Optional[Integer] $listen_port              = undef,
+  Optional[Integer] $local_max_rate           = undef,
+  Optional[String] $pam_service_name          = undef,
   Optional[Boolean] $userlist_deny            = undef,
   Optional[Boolean] $userlist_enable          = undef,
-  $userlist_file                              = undef,
-  $user_config_dir                            = undef,
+  Optional[String] $userlist_file             = undef,
+  Optional[String] $user_config_dir           = undef,
   Optional[Integer] $max_clients              = undef,
   Optional[Integer] $max_per_ip               = undef,
   Optional[Boolean] $syslog_enable            = undef,
   Optional[Boolean] $dual_log_enable          = undef,
   Optional[Boolean] $tcp_wrappers             = undef,
   Optional[Boolean] $use_localtime            = undef,
-  $secure_chroot_dir                          = undef,
+  Optional[String] $secure_chroot_dir         = undef,
   Optional[Boolean] $ascii_download_enable    = undef,
   Optional[Boolean] $ascii_upload_enable      = undef,
-  $rsa_cert_file                              = undef,
-  $rsa_private_key_file                       = undef,
+  Optional[String] $rsa_cert_file             = undef,
+  Optional[String] $rsa_private_key_file      = undef,
   Optional[Boolean] $ssl_enable               = undef,
   Optional[Boolean] $allow_writeable_chroot   = undef,
   Optional[Boolean] $pasv_enable              = undef,
-  $pasv_min_port                              = undef,
-  $pasv_max_port                              = undef,
-  $pasv_address                               = undef,
+  Optional[Integer] $pasv_min_port            = undef,
+  Optional[Integer] $pasv_max_port            = undef,
+  Optional[String] $pasv_address              = undef,
   Optional[Boolean] $allow_anon_ssl           = undef,
   Optional[Boolean] $force_local_data_ssl     = undef,
   Optional[Boolean] $force_local_logins_ssl   = undef,
@@ -93,11 +94,21 @@ class vsftpd (
     ensure => installed,
   }
 
-  ### FIXME file_open_mode check for input
-  ### FIXME check for umask to be octal and check for input
-
+  # check if "chown_username" is used without "chown_uploads"
   if ($chown_username != undef) and ($chown_uploads == false or $chown_uploads == undef) {
     fail('Cannot use "chown_username" without "chown_uploads" set to true')
+  }
+
+
+  # check these umask parameter for validity
+  if $anon_umask != undef and ($anon_umask !~ /^[0-7]?[0-7][0-7][0-7]$/) {
+    fail('umask must be an octal value. F.e. 0022')
+  }
+  if $local_umask != undef and ($local_umask !~ /^[0-7]?[0-7][0-7][0-7]$/) {
+    fail('umask must be an octal value. F.e. 0022')
+  }
+  if $file_open_mode != undef and ($file_open_mode !~ /^[0-7][0-7][0-7][0-7]$/) {
+    fail('file open mode must be an octal value. F.e. 0660')
   }
 
   file { $config_path:
---
+++ spec/classes/string_and_integer_parameters_spec.rb
@@ -0,0 +1,98 @@
+require 'spec_helper'
+
+describe 'vsftpd' do
+  let(:title) { 'vsftpd_conf' }
+  let(:node) { 'test.example.com' }
+
+  context 'Test non-boolean parameters on RHEL' do
+    let(:facts) {
+      {
+        :osfamily => 'RedHat',
+        :operatingsystem => 'CentOS',
+        :architecture => 'x86_64',
+      }
+    }
+    let(:params) {
+      {
+        'anon_max_rate'        => 8192,
+        'ftp_data_port'        => 20,
+        'listen_port'          => 21,
+        'local_max_rate'       => 16384,
+        'max_clients'          => 10,
+        'max_per_ip'           => 1,
+        'pasv_min_port'        => 4000,
+        'pasv_max_port'        => 5000,
+        'anon_root'            => '/var/lib/empty',
+        'ftp_username'         => 'ftp',
+        'guest_username'       => 'nobody',
+        'ftpd_banner'          => 'Welcome to the puppet deployed FTP server',
+        'banner_file'          => '/etc/vsftpd/banner.txt',
+        'chroot_list_file'     => '/etc/vsftpd/chroot_list',
+        'pam_service_name'     => 'ftp',
+        'userlist_file'        => '/etc/vsftpd/user_list',
+        'user_config_dir'      => '/etc/vsftpd/user_conf',
+        'secure_chroot_dir'    => '/usr/share/empty',
+        'rsa_cert_file'        => '/etc/vsftpd/certs/vsftpd.pem',
+        'rsa_private_key_file' => '/etc/vsftpd/certs/vsftpd.key',
+        'pasv_address'         => '127.0.0.1',
+      }
+    }
+
+    # check if every non-boolean parameter works as expected
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').with_content(/^anon_max_rate=8192/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').with_content(/^ftp_data_port=20/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').with_content(/^listen_port=21/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').with_content(/^local_max_rate=16384/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').with_content(/^max_clients=10/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').with_content(/^max_per_ip=1/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').with_content(/^pasv_min_port=4000/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').with_content(/^pasv_max_port=5000/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').with_content(/^anon_root=\/var\/lib\/empty/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').with_content(/^ftp_username=ftp/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').with_content(/^guest_username=nobody/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').with_content(/^ftpd_banner=Welcome to the puppet deployed FTP server/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').with_content(/^banner_file=\/etc\/vsftpd\/banner.txt/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').with_content(/^chroot_list_file=\/etc\/vsftpd\/chroot_list/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').with_content(/^pam_service_name=ftp/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').with_content(/^userlist_file=\/etc\/vsftpd\/user_list/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').with_content(/^user_config_dir=\/etc\/vsftpd\/user_conf/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').with_content(/^secure_chroot_dir=\/usr\/share\/empty/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').with_content(/^rsa_cert_file=\/etc\/vsftpd\/certs\/vsftpd.pem/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').with_content(/^rsa_private_key_file=\/etc\/vsftpd\/certs\/vsftpd.key/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').with_content(/^pasv_address=127\.0\.0\.1/) }
+  end
+
+  context 'Test for non-boolean absent parameters on RHEL' do
+    let(:facts) {
+      {
+        :osfamily => 'RedHat',
+        :operatingsystem => 'CentOS',
+        :architecture => 'x86_64',
+      }
+    }
+
+    # check if every non-boolean parameter is absent 
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').without_content(/^anon_max_rate=/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').without_content(/^ftp_data_port=/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').without_content(/^listen_port=/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').without_content(/^local_max_rate=/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').without_content(/^max_clients=/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').without_content(/^max_per_ip=/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').without_content(/^pasv_min_port=/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').without_content(/^pasv_max_port=/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').without_content(/^anon_root=/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').without_content(/^ftp_username=/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').without_content(/^guest_username=/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').without_content(/^ftpd_banner=/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').without_content(/^banner_file=/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').without_content(/^chroot_list_file=/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').without_content(/^pam_service_name=/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').without_content(/^userlist_file=/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').without_content(/^user_config_dir=/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').without_content(/^secure_chroot_dir=/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').without_content(/^rsa_cert_file=/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').without_content(/^rsa_private_key_file=/) }
+    it { is_expected.to contain_file('/etc/vsftpd/vsftpd.conf').without_content(/^pasv_address=/) }
+  end
+
+end
