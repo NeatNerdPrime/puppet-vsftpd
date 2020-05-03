518c242b3701b835f2be7dc1ea26e0848164a674
Philipp Seiler <p.seiler@linuxmail.org>
2020-05-03 13:17:34 +0200

    new tests for debian, SLES and unsupported operating systems

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -21,4 +21,72 @@ describe 'vsftpd' do
     it { is_expected.to contain_service('vsftpd').with_require('Package[vsftpd]') }
     it { is_expected.to contain_service('vsftpd').with_subscribe('File[/etc/vsftpd/vsftpd.conf]') }
     end
+  context 'Check for basic bevahiour on Debian' do
+    let(:facts) {
+      {
+        :osfamily => 'Debian',
+        :operatingsystem => 'Debian',
+        :architecture => 'x86_64',
+      }
+    }
+
+    it { is_expected.to contain_class('vsftpd::params') }
+    it { is_expected.to compile.with_all_deps }
+
+    it { is_expected.to contain_package('vsftpd') }
+    it { is_expected.to contain_service('vsftpd').with_enable(true) }
+    it { is_expected.to contain_service('vsftpd').with_require('Package[vsftpd]') }
+    it { is_expected.to contain_service('vsftpd').with_subscribe('File[/etc/vsftpd.conf]') }
+    end
+  context 'Check for basic bevahiour on Debian' do
+    let(:facts) {
+      {
+        :osfamily => 'Suse',
+        :operatingsystem => 'SLES',
+        :architecture => 'x86_64',
+      }
+    }
+
+    it { is_expected.to contain_class('vsftpd::params') }
+    it { is_expected.to compile.with_all_deps }
+
+    it { is_expected.to contain_package('vsftpd') }
+    it { is_expected.to contain_service('vsftpd').with_enable(true) }
+    it { is_expected.to contain_service('vsftpd').with_require('Package[vsftpd]') }
+    it { is_expected.to contain_service('vsftpd').with_subscribe('File[/etc/vsftpd.conf]') }
+    end
+  context 'Check if manage_service = false on RHEL works' do
+    let(:facts) {
+      {
+        :osfamily => 'RedHat',
+        :operatingsystem => 'CentOS',
+        :architecture => 'x86_64',
+      }
+    }
+
+    let(:params) {
+      {
+        'manage_service' => false,
+      }
+    }
+
+    it { is_expected.to contain_class('vsftpd::params') }
+    it { is_expected.to compile.with_all_deps }
+
+    it { is_expected.to contain_package('vsftpd') }
+    it { is_expected.not_to contain_service('vsftpd').with_enable(true) }
+    it { is_expected.not_to contain_service('vsftpd').with_require('Package[vsftpd]') }
+    it { is_expected.not_to contain_service('vsftpd').with_subscribe('File[/etc/vsftpd/vsftpd.conf]') }
+    end
+  context 'Check if module fails on an unsupported operating system' do
+    let(:facts) {
+      {
+        :osfamily => 'Solaris',
+        :operatingsystem => 'OpenIndiana',
+        :architecture => 'x86_64',
+      }
+    }
+
+    it { is_expected.to compile.and_raise_error(/OpenIndiana not supported./) }
+    end
 end
